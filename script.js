defd1658167182f47cbe54cae486264516343efc
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-01 22:55:40 -0500

    added comments and deleted some console.log

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+ //get saved tasks from local storage
  var allDayTasks = localStorage.getItem("tasks");
  if (allDayTasks === null){
      allDayTasks ={};
@@ -7,24 +8,25 @@
      allDayTasks = JSON.parse(allDayTasks);
  }
 
- // look for the key with # and show its
+ // look for the key with # and show its value
 for(key in allDayTasks){
-    $("#"+ key).val(allDayTasks[key])   // $("#9")
+    $("#"+ key).val(allDayTasks[key])  
 }
-    
+
+//when save btn is clicked, the following function will go find the button's sibling with class of description
+//get the value of the input and save that to the local storage
+//it also assign an id to its sibling for later use
 $(".saveBtn").click(function(){
-    //console.log(this);
     var taskContent = $(this).siblings(".description").val();
     var time = $(this).siblings(".description").attr("id");
     allDayTasks[time]=taskContent
-    console.log(allDayTasks)
-
     localStorage.setItem("tasks", JSON.stringify(allDayTasks))
 })
 
+//using moment.js, compare the time of each row to the current
+//if it is smaller than the current, it is the past -> the past time blocks have gray color
+//if it it larger than the current, it is the future --> the future time blocks have green color
 var current = moment().hours();
-console.log (current)
-current=13;
 $(".description").each(function(){
     var time = parseInt($(this).attr("id"))
     if (time < current){
